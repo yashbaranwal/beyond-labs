b3791fe58fc384e4d4134262694d36b4ea94ca33
yashbaranwal <yashbaranwalwithu@gmail.com>
2025-06-25 15:39:09 +0530

    fix styling for textarea

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -7,7 +7,10 @@ function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
     <textarea
       data-slot="textarea"
       className={cn(
-        "border-gray placeholder:text-foreground/40 focus-visible:border focus-visible:ring-gray focus-visible:ring-[1px] hover:ring-[3px] hover:ring-primary/7 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-sm text-foreground/60 font-medium shadow-xs transition-[color,box-shadow] outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "border border-gray placeholder:text-foreground/40 dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md bg-transparent px-3 py-2 text-sm text-foreground/60 font-medium transition-[color,box-shadow] outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "hover:ring-[3px] hover:ring-primary/7 hover:border-primary/7",
+        "focus-visible:border focus-visible:border-gray focus-visible:shadow-[0px_1px_2px_0px_rgba(0,0,0,0.05)] focus-visible:ring-0",
+        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
         className
       )}
       {...props}
